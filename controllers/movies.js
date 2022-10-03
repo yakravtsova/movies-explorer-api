85c62293cae8fb0aa8d0ff8655baa8498a26ccf9
Margarita Kravtsova <margaritaselez@yandex.ru>
2022-10-03 15:01:25 +0300

    выводятся только фильмы текущего пользователя

--- controllers/movies.js
+++ controllers/movies.js
@@ -38,7 +38,7 @@ const deleteMovie = (req, res, next) => {
 };
 
 const getMovies = (req, res, next) => {
-  Movie.find({})
+  Movie.find({ owner: req.user._id })
     .then((movies) => res.status(200).send(movies))
     .catch(next);
 };
